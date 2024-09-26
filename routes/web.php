9ddf28bc070cf560416a5f4966af1a43c5250ee6
thuantv414 <tranthuan142004@gmail.com>
2024-09-26 22:13:14 +0700

    giao dien admin

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::resource('/',ShopController::class);
+
+Route::get('/admin/danhmucs', function () {
+    return view('admins.danhmucs.index');
+});
+Route::resource('/', ShopController::class);
