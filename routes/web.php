8a22aa9da8bf22edaab8ecfd59ab31e4ce8f7c3e
Nguyen-Trung-Kien2309 <nkien9050@gmail.com>
2024-09-26 22:42:13 +0700

    lịch sử đặt hànghàng

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Clients\ProfileController;
 use App\Http\Controllers\Clients\ShopController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,4 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 Route::resource('/',ShopController::class);
+Route::resource('/profile',ProfileController::class);
